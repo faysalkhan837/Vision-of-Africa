7fcdd3bbf49276b62fca74a5774d07329a3cab51
faysalkhan837 <faysal.khan837@gmail.com>
2024-05-28 03:23:00 +0600

    2nd comit

--- src/Component/Flags/Flags.jsx
+++ src/Component/Flags/Flags.jsx
@@ -3,9 +3,9 @@ import './flags.css';
 const Flags = ({flag}) => {
     const {nameeng, flagImg} = flag;
     return (
-        <div className="flex flex-col items-center w-36 h-36 mx-auto text-center text-white font-semibold space-y-5">
-           <div className=''>
-                <img className="w-[132px] h-20" src={flagImg}  />
+        <div className="flex flex-col items-center w-40 h-36 mx-auto text-center text-white font-semibold space-y-5">
+           <div className='boxshado'>
+                <img className="w-36 h-20" src={flagImg}  />
            </div>
            <h1 className="leading-3">{nameeng}</h1> 
         </div>
--- src/Page/Home/Home.jsx
+++ src/Page/Home/Home.jsx
@@ -4,16 +4,16 @@ import Flags from '../../Component/Flags/Flags';
 
 const Home = () => {
     const [flags, setFlags] = useState([])
-    useEffect(()=>{
+    useEffect(() => {
         fetch("flags.json")
-        .then(res => res.json())
-        .then(data => setFlags(data))
-    },[])
+            .then(res => res.json())
+            .then(data => setFlags(data))
+    }, [])
 
     return (
-        <div>           
+        <div>
             <div className='relative z[1]'>
-            <video src="https://vs-africa.org/wp-content/uploads/2024/03/Vision-for-Sustainable-Africa.mp4" autoPlay muted loop controls controlsList="nodownload" className="overflow-hidden "></video>
+                <video src="https://vs-africa.org/wp-content/uploads/2024/03/Vision-for-Sustainable-Africa.mp4" autoPlay muted loop controls controlsList="nodownload" className="overflow-hidden "></video>
             </div>
             <div className='text-center leading-[45px]'>
                 <h1 className='text-[50px] font-bold text-[#016C36] px-16 py-14'>Together, for a sustainable future in Africa</h1>
@@ -21,12 +21,15 @@ const Home = () => {
             <div className='text-center leading-[45px] bg-[#E3DDD4]'>
                 <h1 className='text-[50px] font-bold text-[#016C36] px-16 py-14'>SDG Ranking of African Region</h1>
             </div>
-            <div className='paralux'>
-                <div className=' bg-[#1D4055] opacity-85'>                  
-                    <div className='grid grid-cols-8 p-4'>
-                         {
-                             flags.map(flag => <Flags key={flag.nameeng} flag={flag} ></Flags>)
-                         }
+            <div className='parallax'>
+                <div className=' overlay'>
+                    <div className='content'>
+                        <h1 className='text-[50px] font-bold py-5'>Sustainability and Climate Action of African Region</h1>
+                        <div className='grid grid-cols-8 px-4 py-10 gap-y-5'>
+                            {
+                                flags.map(flag => <Flags key={flag.nameeng} flag={flag} ></Flags>)
+                            }
+                        </div>
                     </div>
                 </div>
             </div>
